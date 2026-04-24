39ee14df40a961cdd10519bf793b44658709fa85
yenugantirahul <yenugantirahul@gmail.com>
2026-04-24 11:39:26 +0530

    middleware.ts deleted

--- client/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: [
-    "/((?!_next|.*\\..*).*)", // protect everything except static files
-  ],
-};
--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
+
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
